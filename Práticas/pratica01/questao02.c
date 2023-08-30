69e0427f1e5a287e0b9c3c25af0cf34b20c94cad
GuCastroMiranda <142408250+GuCastroMiranda@users.noreply.github.com>
2023-08-30 15:41:53 +0000

    aula prof jose reginaldo

---
+++ Práticas/pratica01/questao02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+  int main(){
+  float base = 7;
+    float altura = 7;
+
+    float area = base*altura / 2;
+
+    printf("A area do triangulo da Julia é %f\n", area);
+    
+  return 0;
+  
+}
---
+++ Práticas/pratica01/questao03.c
@@ -0,0 +1,9 @@
+//Faça um programa em C que calcule o perímetro de uma pizza (P = 2 x PI x r). Considere o raio um número inteiro e a constante PI igual a 3,1416.//
+#include <stdio.h>
+int main() {
+int raio = 10;
+  float pi = 3.1416;
+ float perimetro = 2*raio*pi;
+  printf("O perimetro da pizza é %f\n", perimetro);
+  return 0;
+}
